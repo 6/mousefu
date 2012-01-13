bb7620d9e90b99a2ab67f627819a543519375f73
Peter Graham <peterghm@gmail.com>
2012-01-12 23:42:48 -0500

    better code organization, #1

--- mousefu.coffee
+++ mousefu.coffee
@@ -12,16 +12,57 @@ class Util
   between: (num, min, max) ->
     Math.min(max, Math.max(min, num))
 
+class MouseFu
+  monitored_events: {}
+  has_bindings: {}
+  
+  event_detected: ($h, detected_event_s, event_obj) ->
+    for i, events_info of @monitored_events[$h]
+      for event_s in events_info.list
+        if detected_event_s is event_s
+          @monitored_events[$h][i].state[detected_event_s] = event_obj
+    @fire_callbacks $h
+
+  add_monitored_events: ($h, events_info) ->
+    @monitored_events[$h] ?= []
+    @monitored_events[$h].push events_info
+
+  fire_callbacks: ($h) ->
+    for i, events_info of @monitored_events[$h]
+      # if event detected for all monitored events
+      if Object.keys(events_info.state).length is events_info.list.length
+        @send_to_cb $h, events_info
+        # flush state
+        @monitored_events[$h][i].state = {}
+    
+  send_to_cb: ($h, events_info) ->
+    coords = {}
+    for event_s in events_info.list
+      coords[event_s] = u.relative_coords($h, events_info.state[event_s])
+    events_info.cb coords
+  
+  '''flush_with: ($h, event_s) ->
+    for i, events_info of @monitored_events[$h]
+      if $.inArray(event_s, events_info.list) > -1
+        #TODO flush the whole state?
+        delete @monitored_events[$h][i].state[event_s]'''
+
 u = new Util
+m = new MouseFu
 
 $.fn.extend
-  mousefu_hover: (fn) ->
-    $(@).mousemove (e) -> fn u.relative_coords($(@), e)
-  
-  mousefu_hover_in: (fn) ->
-    $(@).mouseenter (e) -> fn u.relative_coords($(@), e)
-  
-  mousefu_hover_out: (fn) ->
-    $(@).mouseleave (e) -> fn u.relative_coords($(@), e)
+  mousefu: (events_s, cb) ->   
+    events_s_list = events_s.split(' ')
+    #TODO check validity of event strings
+    m.add_monitored_events $(@), {list: events_s_list, cb: cb, state: {}}
+ 
+    # only bind events once per $(@)
+    return if m.has_bindings[$(@)]?
     
-
+    $(@).mouseenter (e) => m.event_detected $(@), 'enter', e
+    $(@).mouseleave (e) => m.event_detected $(@), 'leave', e
+    $(@).mousemove (e) => m.event_detected $(@), 'move', e
+    $(@).mousedown (e) => m.event_detected $(@), 'down', e
+    $(@).mouseup (e) => m.event_detected $(@), 'up', e
+    
+    m.has_bindings[$(@)] = yes
--- mousefu.js
+++ mousefu.js
@@ -1,5 +1,6 @@
 (function() {
-  var Util, u;
+  var MouseFu, Util, m, u;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   Util = (function() {
     function Util() {}
     Util.prototype.relative_coords = function($h, e) {
@@ -22,22 +23,85 @@
     };
     return Util;
   })();
+  MouseFu = (function() {
+    function MouseFu() {}
+    MouseFu.prototype.monitored_events = {};
+    MouseFu.prototype.has_bindings = {};
+    MouseFu.prototype.event_detected = function($h, detected_event_s, event_obj) {
+      var event_s, events_info, i, _i, _len, _ref, _ref2;
+      _ref = this.monitored_events[$h];
+      for (i in _ref) {
+        events_info = _ref[i];
+        _ref2 = events_info.list;
+        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+          event_s = _ref2[_i];
+          if (detected_event_s === event_s) {
+            this.monitored_events[$h][i].state[detected_event_s] = event_obj;
+          }
+        }
+      }
+      return this.fire_callbacks($h);
+    };
+    MouseFu.prototype.add_monitored_events = function($h, events_info) {
+      var _base, _ref;
+      if ((_ref = (_base = this.monitored_events)[$h]) == null) {
+        _base[$h] = [];
+      }
+      return this.monitored_events[$h].push(events_info);
+    };
+    MouseFu.prototype.fire_callbacks = function($h) {
+      var events_info, i, _ref, _results;
+      _ref = this.monitored_events[$h];
+      _results = [];
+      for (i in _ref) {
+        events_info = _ref[i];
+        _results.push(Object.keys(events_info.state).length === events_info.list.length ? (this.send_to_cb($h, events_info), this.monitored_events[$h][i].state = {}) : void 0);
+      }
+      return _results;
+    };
+    MouseFu.prototype.send_to_cb = function($h, events_info) {
+      var coords, event_s, _i, _len, _ref;
+      coords = {};
+      _ref = events_info.list;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        event_s = _ref[_i];
+        coords[event_s] = u.relative_coords($h, events_info.state[event_s]);
+      }
+      return events_info.cb(coords);
+    };
+    'flush_with: ($h, event_s) ->\nfor i, events_info of @monitored_events[$h]\n  if $.inArray(event_s, events_info.list) > -1\n    #TODO flush the whole state?\n    delete @monitored_events[$h][i].state[event_s]';
+    return MouseFu;
+  })();
   u = new Util;
+  m = new MouseFu;
   $.fn.extend({
-    mousefu_hover: function(fn) {
-      return $(this).mousemove(function(e) {
-        return fn(u.relative_coords($(this), e));
+    mousefu: function(events_s, cb) {
+      var events_s_list;
+      events_s_list = events_s.split(' ');
+      m.add_monitored_events($(this), {
+        list: events_s_list,
+        cb: cb,
+        state: {}
       });
-    },
-    mousefu_hover_in: function(fn) {
-      return $(this).mouseenter(function(e) {
-        return fn(u.relative_coords($(this), e));
-      });
-    },
-    mousefu_hover_out: function(fn) {
-      return $(this).mouseleave(function(e) {
-        return fn(u.relative_coords($(this), e));
-      });
+      if (m.has_bindings[$(this)] != null) {
+        return;
+      }
+      $(this).mouseenter(__bind(function(e) {
+        return m.event_detected($(this), 'enter', e);
+      }, this));
+      $(this).mouseleave(__bind(function(e) {
+        return m.event_detected($(this), 'leave', e);
+      }, this));
+      $(this).mousemove(__bind(function(e) {
+        return m.event_detected($(this), 'move', e);
+      }, this));
+      $(this).mousedown(__bind(function(e) {
+        return m.event_detected($(this), 'down', e);
+      }, this));
+      $(this).mouseup(__bind(function(e) {
+        return m.event_detected($(this), 'up', e);
+      }, this));
+      return m.has_bindings[$(this)] = true;
     }
   });
 }).call(this);
